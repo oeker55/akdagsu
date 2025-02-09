4f134fd4b1662e783c5a56c7854cceee911cf4ed
oeker55 <oeker55@gmail.com>
2025-02-09 14:28:12 +0300

    error kaldırldı

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -12,7 +12,7 @@ export const generateToken = (userId: string) => {
 export const verifyToken = (token: string) => {
   try {
     return jwt.verify(token, JWT_SECRET);
-  } catch (error) {
+  } catch {
     return null;
   }
 }; 
